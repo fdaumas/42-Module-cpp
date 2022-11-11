697eb0e74abbfe92272658c272a4afaed239bf32
Florian Daumas <fdaumas@z2r5p8.42lyon.fr>
2022-11-11 16:41:36 +0100

    cpp-01 ex00 complete

---
+++ cpp_00/ex00/megaphone.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+void str_toupper(char *str)
+{
+	int index = 0;
+	while (str[index])
+		std::cout << (char)toupper(str[index++]);
+}
+
+int main(int argc, char *argv[])
+{
+	int index_arg = 1;
+
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	while(index_arg < argc) {
+		str_toupper(argv[index_arg]);
+		index_arg++;
+	}
+	std::cout << std::endl;
+	return (0);
+}
