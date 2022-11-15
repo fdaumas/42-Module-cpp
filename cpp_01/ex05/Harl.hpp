e0eee535562ad62546af18f5a338f5d544e14e3d
Florian Daumas <fdaumas@z2r4p5.42lyon.fr>
2022-11-15 21:05:59 +0100

    cpp_01 exercise 05 complete

---
+++ cpp_01/ex05/Harl.hpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <map>
+
+class Harl {
+public:
+	Harl(void);
+	~Harl(void);
+
+	void complain(std::string level);
+
+private:
+	void debug(void);
+	void info(void);
+	void warning(void);
+	void error(void);
+};
+
+typedef std::map<std::string, void(Harl::*)(void)> my_map;
