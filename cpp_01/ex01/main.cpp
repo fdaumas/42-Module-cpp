9ffdd2672bbe6ee8a95992267b9648ca15d8e379
Florian Daumas <fdaumas@z1r12p3.42lyon.fr>
2022-11-15 17:52:18 +0100

    better main

--- cpp_01/ex01/main.cpp
+++ cpp_01/ex01/main.cpp
@@ -1,7 +1,15 @@
 #include "Zombie.hpp"
 
 int main(void) {
-	Zombie* zombiehorde = zombieHorde(42, "Zombie");
+	int n_zombie = 42;
+	Zombie *zombiehorde = zombieHorde(n_zombie, "ZOMBIE");
+
+	int index = 0;
+	while (index < n_zombie) {
+		std::cout << index << " ";
+		zombiehorde[index++].announce();
+	}
+
 	delete [] zombiehorde;
 	return (0);
 }
