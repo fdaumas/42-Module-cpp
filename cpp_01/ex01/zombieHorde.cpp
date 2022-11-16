2ac43e5ad0152b1c094b10c47d05205d5ecf083b
fdaumas <florian.daumas.git@gmail.com>
2022-11-16 12:28:50 +0100

    secure for N zombie negative

--- cpp_01/ex01/zombieHorde.cpp
+++ cpp_01/ex01/zombieHorde.cpp
@@ -1,6 +1,10 @@
 #include "Zombie.hpp"
 
 Zombie* zombieHorde(int N, std::string name) {
+	if (N <= 0) {
+		std::cout << "you can't have negative number of zombie" << std::endl;
+		exit(1);
+	}
 	Zombie* zombiehorde = new Zombie[N];
 	int index = 0;
 	while (index < N) {
