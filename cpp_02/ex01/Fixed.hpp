b88e122b5c32a8bfecb4c599ea9bddb866f3f990
fdaumas <florian.daumas.git@gmail.com>
2022-11-17 08:47:46 +0100

    cpp_02 ex01 complete

--- cpp_02/ex01/Fixed.hpp
+++ cpp_02/ex01/Fixed.hpp
@@ -1,15 +1,23 @@
 #include <iostream>
+#include <cmath>
 
 class Fixed {
 public:
 	Fixed();
+	Fixed(int int_number);
+	Fixed(float float_number);
 	Fixed(const Fixed &fixed);
 	~Fixed();
 	Fixed&operator=(const Fixed &fixed);
+
 	int getRawBits(void) const;
 	void setRawBits(int const raw);
+	float toFloat( void ) const;
+	int toInt( void ) const;
 
 private:
 	int _number ;
 	const static int _number_fractional_bits = 8;
 };
+
+std::ostream& operator<<(std::ostream& out, const Fixed& fixed);
