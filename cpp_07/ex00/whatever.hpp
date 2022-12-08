8d70c64d7cb1469d120ba4dc00b4a7acd89674c6
fdaumas <florian.daumas.git@gmail.com>
2022-12-08 10:22:40 +0100

    WTF ehat is this ?????????

--- cpp_07/ex00/whatever.hpp
+++ cpp_07/ex00/whatever.hpp
@@ -3,6 +3,19 @@
 
 #include <iostream>
 
-template<class T>
+template<class  T>
+void swap(T& a, T& b) {
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+template<class  T>
+T const & min(T const & a, T const & b) {
+	return (b <= a ? b : a);
+}
+template<class  T>
+T const & max(T const & a, T const & b) {
+	return (b >= a ? b : a);
+}
 
 #endif
