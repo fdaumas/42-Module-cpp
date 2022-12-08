f4a48708640391f8bfa77f420d35effe95f4d47c
fdaumas <florian.daumas.git@gmail.com>
2022-12-08 13:44:50 +0100

    complete work

--- cpp_07/ex01/iter.hpp
+++ cpp_07/ex01/iter.hpp
@@ -1,13 +1,19 @@
 #ifndef _ITER_H_
 # define _ITER_H_
 
+#include <iostream>
+
 template<typename T>
 void print(T const & value) {
 	std::cout << value << std::endl;
 }
 template<typename T>
-void iter(T * list, int length, void *f(T const & value)) {
-
+void iter(T * list, int length, void f(T const & value)) {
+	int index = 0;
+	while (index < length) {
+		f(list[index]);
+		index++;
+	}
 }
 
 #endif
--- cpp_07/ex01/main.cpp
+++ cpp_07/ex01/main.cpp
@@ -1,5 +1,9 @@
 #include "iter.hpp"
 
 int main() {
+	int int_arr[] = {0, 1, 2, 3, 4};
+	iter(int_arr, 5, print);
+	std::string string_arr[] = {"a", "b", "c"};
+	iter(string_arr, 3, print);
 	return (0);
 }
