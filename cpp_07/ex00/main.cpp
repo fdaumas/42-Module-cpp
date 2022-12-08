8941d0503029a989bff0183e170c5ab8a83bea98
fdaumas <florian.daumas.git@gmail.com>
2022-12-08 10:13:03 +0100

    swap work correctly

---
+++ cpp_07/ex00/main.cpp
@@ -0,0 +1,26 @@
+#include "whatever.hpp"
+
+void swap(T& a, T& b) {
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+T const & min(T const & a, T const & b) {
+	return (b >= a ? b : a);
+}
+
+int main( void ) {
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+//	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+//	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+//	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+//	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+}
---
+++ cpp_07/ex00/whatever.hpp
@@ -0,0 +1,8 @@
+#ifndef _WHATEVER_H_
+# define _WHATEVER_H_
+
+#include <iostream>
+
+template<class T>
+
+#endif
